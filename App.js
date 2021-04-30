007848a90f31525929b0db6159caaf70f10c7162
Pulak Bhawsar <pulak.bhawsar@competegaming.com>
2021-04-30 11:25:58 +0530

    Initial commit for WebGL sample project, facing erroron npm start: ReferenceError: Can't find variable: document

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  document
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Unity from 'react-native-unity-webgl';
+
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Unity
+        src="Public/Build/myGame.json"
+        loader="Public/Build/UnityLoader.js"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
